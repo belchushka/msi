05e669e3b5db1c67eddfc6ff836e94eb50367527
belchushka <savvashu@gmail.com>
2023-05-28 11:03:43 +0300

    news card

--- src/pages/news/ui.tsx
+++ src/pages/news/ui.tsx
@@ -1,62 +1,99 @@
 import {SafeView} from '@/shared/ui/safeView';
 import React, {useEffect, useState} from 'react';
 import {useNavigation, useTheme} from '@react-navigation/native';
-import { $host } from '@/shared/api';
-import { ActivityIndicator, Image, ScrollView, Text, TouchableOpacity, View } from 'react-native';
+import {$host} from '@/shared/api';
+import {
+  ActivityIndicator,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import styles from './styles';
-import { Container } from '@/shared/ui';
+import {Container} from '@/shared/ui';
 
+interface INewsCard {
+  newsElement: any;
+}
+
+const NewsCard: React.FC<INewsCard> = ({newsElement}) => {
+  const [opened, setOpened] = useState(false);
+  return (
+    <View style={styles.news_container}>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <Image
+          style={{width: 50, height: 50, borderRadius: 50}}
+          source={{
+            uri: 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww&w=1000&q=80',
+          }}
+        />
+        <Text
+          style={{
+            color: 'black',
+            fontSize: 20,
+            paddingLeft: 8,
+            fontWeight: '500',
+          }}>
+          {newsElement.authorname}
+        </Text>
+      </View>
+      <Text style={styles.news_text}>
+        {newsElement.text.length > 200
+          ? !opened
+            ? newsElement.text.split(' ').slice(0, 20).join(' ') + '...'
+            : newsElement.text
+          : newsElement.text}
+      </Text>
+      <TouchableOpacity onPress={() => setOpened(state => !state)}>
+        <Text style={{color: '#A4CE57', fontSize: 20, paddingBottom: 20}}>
+          {!opened ? 'Читать далее' : 'Свернуть'}
+        </Text>
+      </TouchableOpacity>
+      <Text style={{color: '#A7A9AB'}}>{newsElement.createdAt}</Text>
+      {newsElement.image == null || newsElement.image.length < 10 ? null : (
+        <Image
+          style={{height: 200, width: 340}}
+          source={{uri: newsElement.image}}
+        />
+      )}
+    </View>
+  );
+};
 
 export const NewsPage = () => {
-  
-    const [news, setNews] = useState([]);
+  const [news, setNews] = useState([]);
 
-  const theme = useTheme();
-  const navigation = useNavigation();
-
-  useEffect(()=>{
-    (async ()=>{
+  useEffect(() => {
+    (async () => {
       const {data} = await $host.get('/feed/allposts');
-      console.log(data)
-      setNews(data.data)
-    })()
-  },[])
+      console.log(data);
+      setNews(data.data);
+    })();
+  }, []);
 
-  if (!news){
-    return <ActivityIndicator/>
+  if (!news) {
+    return <ActivityIndicator />;
   }
 
   return (
     <SafeView style={{flex: 1}}>
       <Container>
         <Text style={styles.header}>Новости</Text>
-        <ScrollView contentContainerStyle={{
-          paddingVertical: 20,
-          gap: 20,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          flexWrap: 'wrap', 
-        }}>
-
-        {news.map((newsElement, i)=>{
-            return <View style={styles.news_container}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <Image style={{width: 50, height: 50, borderRadius: 50}} source={{uri: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fHww&w=1000&q=80"}}/>
-                    <Text style={{color: 'black', fontSize: 20, paddingLeft: 8, fontWeight: '500'}}>{newsElement.authorname}</Text>
-                </View>
-                <Text style={styles.news_text}>{newsElement.text.length > 200 ? newsElement.text.split(" ").slice(0, 20).join(" ")+"..." : newsElement.text}</Text>
-                <TouchableOpacity>
-                    <Text style={{color: '#A4CE57', fontSize: 20, paddingBottom: 20}}>Читать далее</Text>
-                </TouchableOpacity>
-                <Text style={{color: '#A7A9AB'}}>{newsElement.createdAt}</Text>
-                {newsElement.image == null || newsElement.image.length < 10 ? null : <Image style={{height: 200, width: 340}} source={{uri: newsElement.image}}/>}
-            </View>
-        })}
-            
+        <ScrollView
+          contentContainerStyle={{
+            paddingVertical: 20,
+            gap: 20,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            flexWrap: 'wrap',
+          }}>
+          {news.map((newsElement, i) => {
+            return <NewsCard key={i} newsElement={newsElement} />;
+          })}
+          ;
         </ScrollView>
       </Container>
-        
-      
     </SafeView>
   );
 };
