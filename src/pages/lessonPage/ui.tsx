393c0c8fe20b84afc22d1938425f47a9a9a28ffc
belchushka <savvashu@gmail.com>
2023-05-28 11:16:36 +0300

    news card

--- src/pages/lessonPage/ui.tsx
+++ src/pages/lessonPage/ui.tsx
@@ -1,30 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './styles';
-import { useTheme } from '@/shared/theme';
-import { ActivityIndicator, ScrollView, Text, TouchableOpacity, View, Image } from 'react-native';
-import { SafeView } from '@/shared/ui/safeView';
-import { Button } from '@/shared/ui';
-
+import {Text, View} from 'react-native';
+import {SafeView} from '@/shared/ui/safeView';
+import {Button} from '@/shared/ui';
+import Video from 'react-native-video';
 
 export const LessonPage = ({route}) => {
-
-  const { lesson } = route.params;
-
-  console.log(lesson)
-
-  const theme = useTheme();
+  const {lesson} = route.params;
 
   return (
     <SafeView style={{flex: 1}}>
       <View style={styles.top}>
-        
+        <Video source={{uri: lesson.video}} />
       </View>
 
       <View style={styles.main}>
         <Text style={styles.status}>Пройдено</Text>
         <Text style={styles.header}>{lesson.title}</Text>
         <Text style={styles.description}>{lesson.description}</Text>
-        <Button style={{}} variant='primary'>К следующему уроку</Button>
+        <Button style={{}} variant="primary">
+          К следующему уроку
+        </Button>
       </View>
     </SafeView>
   );
